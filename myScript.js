50a6effcad82ad9c79fc7d5369fecacc5a65b0d0
Samuel Gerhartinger <samuel.g.anmeldung@gmail.com>
2021-02-23 12:04:20 +0100

    Co-authored-by: EliasHoff <EliasHoff@users.noreply.github.com>

--- myScript.js
+++ myScript.js
@@ -20,5 +20,18 @@ a.addEventListener("click", dispJob, false);
 function dispJob() {
     let count = Math.floor(Math.random() * jobs.length);
     console.log(count);
-    document.getElementById('data').innerHTML = JSON.stringify(jobs[count]);
+    //document.getElementById('data').innerHTML = JSON.stringify(jobs[0]);
+    document.getElementById('data').innerHTML = `
+        <a href="${jobs[0].Url}"><h2> ${jobs[0].Titel} </h2></a>
+        <h5> ${jobs[0].CourseId} </h5>
+        <div>${jobs[0].Place}, ${jobs[0].Province}</div>
+        <button id="showDesc">What to do</button>
+        <div id="desc">${jobs[0].Description}</div>
+        `
+    document.getElementById('desc').style.display = 'none';
+    let b = document.getElementById("showDesc");
+    b.addEventListener("click", showDesc, false);
+}
+function showDesc() {
+    document.getElementById('desc').style.display = 'block';
 }
