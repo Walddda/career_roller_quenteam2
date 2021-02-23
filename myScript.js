77ca990c06e9a3831492acc12aeaf6f4e0331d9e
Samuel Gerhartinger <samuel.g.anmeldung@gmail.com>
2021-02-23 15:24:09 +0100

    nearly final
    Co-authored-by: EliasHoff <EliasHoff@users.noreply.github.com>

--- myScript.js
+++ myScript.js
@@ -21,17 +21,45 @@ function dispJob() {
     let count = Math.floor(Math.random() * jobs.length);
     console.log(count);
     //document.getElementById('data').innerHTML = JSON.stringify(jobs[0]);
-    document.getElementById('data').innerHTML = `
-        <a href="${jobs[0].Url}"><h2> ${jobs[0].Titel} </h2></a>
-        <h5> ${jobs[0].CourseId} </h5>
-        <div>${jobs[0].Place}, ${jobs[0].Province}</div>
-        <button id="showDesc">What to do</button>
-        <div id="desc">${jobs[0].Description}</div>
-        `
+    /*
+    CourseId
+    Titel
+    Province
+    Description
+    Url
+    CourseDomain
+    Place
+    CourseDates
+        CourseDateId
+        StartDate
+        CourseType
+        EndDate
+        StartTime
+        EndTime
+        Zip
+        Street
+        Organiser
+        Duration
+        Url
+    */
+    document.getElementById('urlA').href = jobs[count].Url;
+    document.getElementById('title').innerHTML = jobs[count].Titel;
+    document.getElementById('ort').innerHTML = '<span class="iconify" data-icon="simple-line-icons:location-pin" data-inline="false"></span> '+jobs[count].Place +', '+ jobs[count].Province;
+    document.getElementById('id').innerHTML = jobs[count].CourseId;
+    document.getElementById('desc').innerHTML = jobs[count].Description;
+
+    
     document.getElementById('desc').style.display = 'none';
+    document.getElementById('showDesc').style.display = 'block';
+
     let b = document.getElementById("showDesc");
     b.addEventListener("click", showDesc, false);
 }
+
 function showDesc() {
-    document.getElementById('desc').style.display = 'block';
+    if (document.getElementById('desc').style.display == 'block') {
+        document.getElementById('desc').style.display = 'none'
+    }else{
+        document.getElementById('desc').style.display = 'block';
+    }
 }
